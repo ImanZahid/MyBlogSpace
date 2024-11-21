21d5c72c6c56b6e747eb3f9fe4b08e5bd5e981f5
ImanZahid <imanzahidzahid@gmail.com>
2024-11-22 00:33:27 +0300

    Fixes

--- BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
+++ BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3849643be4065489aa7e0b51bad487bfcda432a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
